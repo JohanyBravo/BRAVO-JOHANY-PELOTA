b869ccce6b7725b20f1eae99433442db4fd5d032
Johany Bravo <mayli.bravo@ucuenca.edu.ec>
2023-09-25 16:55:18 -0500

    se le agregó color al fondo y movimiento a la pelota

--- sketch.js
+++ sketch.js
@@ -1,13 +1,43 @@
+let posX;
+let posY;
+let diametro;
+let radio;
+let fondo;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+  diametro = 30;
+  radio = diametro / 2;
+  fondo = color(25, 100, 50);
+  // frameRate(4);
+  // print(posX);
+  // posX = 234;
+  // print(posX);
+  // posX = posX + 25;
+  // print(posX);
+  // posX = posX * 2;
+  // print(posX);
+  // print(posX) = posX / 5;
+  // print(posX);
+  // posX *= 8;
+  // print(posX);
+  // postX += 1;
+  // print(posX);
 }
 
 function draw() {
-  background(153, 50, 204);
-  fill(mouseX);
-  strokeWeight(mouseY);
-  triangle(0, 75, 58, 20, 86, 75);
-  textSize(20);
-  text(mouseX, 50, 50);
-  text(int(mouseY), 50, 100);
- }
+  background(fondo);
+
+  posX -= 10;
+  if (posX < -radio) {
+    diametro = random(10, 100);
+    radio = diametro / 2;
+    fondo = color(255, random(100, 255), 50);
+    posX = windowWidth + radio;
+  }
+  fill(0);
+  circle(posX, posY, diametro);
+  print(posX);
+}

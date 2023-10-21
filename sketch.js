4b6c0c967abf1e7301fa7f5a43c89505e83cc7f7
Johany Bravo <mayli.bravo@ucuenca.edu.ec>
2023-10-20 19:23:25 -0500

    Se cambió de nombre

--- sketch.js
+++ sketch.js
@@ -3,14 +3,17 @@ let posY;
 let diametro;
 let radio;
 let fondo;
-
+let vel;
+let bolita;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   posX = windowWidth / 2;
   posY = windowHeight / 2;
   diametro = 30;
   radio = diametro / 2;
-  fondo = color(25, 100, 50);
+  fondo = color(255, 200, 50);
+  bolita = color(30, 40, 255, 90);
+
   // frameRate(4);
   // print(posX);
   // posX = 234;
@@ -25,6 +28,7 @@ function setup() {
   // print(posX);
   // postX += 1;
   // print(posX);
+  
 }
 
 function draw() {
@@ -35,9 +39,12 @@ function draw() {
     diametro = random(10, 100);
     radio = diametro / 2;
     fondo = color(255, random(100, 255), 50);
+    fill(255, random(0,255), 80);
+    
     posX = windowWidth + radio;
   }
-  fill(0);
+  noStroke();
   circle(posX, posY, diametro);
   print(posX);
+
 }
